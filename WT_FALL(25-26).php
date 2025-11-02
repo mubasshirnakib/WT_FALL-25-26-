5aeeeea532d131d1d3c1381e6c324db350cf3f70
mubasshirnakib <mubasshirnakib@gmail.com>
2025-11-02 14:43:08 +0600

    Form

---
+++ WT_FALL(25-26).php
@@ -0,0 +1,64 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>LAB TASK</title>
+    <style>
+        div{
+            padding-left:80px;
+            border:3px solid black;
+            border-radius:5px;
+        }
+        input[type='submit']{
+            background-color:purple;
+            color:white;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>Clinic Patient Registration</h1>
+</center>
+<center>
+ <table>
+ <tr><td>    
+    Enter your Full Name :</td><td>
+    <input type="text">
+</td></tr> 
+<tr><td> 
+     Enter your Age :</td><td>
+    <input type="text">
+</td></tr> 
+<tr><td> 
+      Enter your Phone Number:</td><td>
+    <input type="text">
+</td></tr> 
+<tr><td> 
+     Enter your Email Address :</td><td>
+    <input type="text">
+</td></tr> 
+<tr><td> 
+     Insurance provider :
+    </td></tr> 
+    <tr><td>
+<select>
+    <option>Select Provider</option>
+    <option>Meghna Life Insurance Company Limited</option>
+    <option>Bangladesh National Insurance Company Limited</option>
+    <option>Pragati Insurance limited</option>
+    </select>
+    </tr></td>
+<tr><td>
+    Insurance Policy Number:</td><td>
+        <input type= "text"></td></tr>
+    </center>  
+</body>
+
+
+<body>
+    <center>
+        <h1>Additional Information</h1>
+</center>
+</body>
+</html>
